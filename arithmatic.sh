8809a0ee5350eaa354f45702c839213be6b6a44b
Sopan <sopan280@gmail.com>
2020-06-15 17:27:33 +0530

    uc5 added

--- arithmatic.sh
+++ arithmatic.sh
@@ -5,5 +5,5 @@ read -p "enter the value of c " c
 #echo "value of a="$a 
 #echo "value of b="$b
 #echo "value of c="$c
-temp=$(( $a*$b+$c ))
-echo "a*b+c="$temp
+temp=$(( $c%$b+$c ))
+echo "a%b+c="$temp
